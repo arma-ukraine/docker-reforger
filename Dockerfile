384753ffcf26bed9ebd85c76bae66296942a7ab8
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2023-06-17 19:48:32 +0000

    simplify, refactor, cleanup, remove unneeded functionality

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,15 @@ SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 RUN apt-get update \
     && \
     apt-get install -y --no-install-recommends --no-install-suggests \
-        python3 \
-        lib32stdc++6 \
-        lib32gcc-s1 \
-        wget \
-        ca-certificates \
-        libcurl4 \
-        net-tools \
-        libssl1.1 \
-        wamerican \
+    python3 \
+    lib32stdc++6 \
+    lib32gcc-s1 \
+    wget \
+    ca-certificates \
+    libcurl4 \
+    net-tools \
+    libssl1.1 \
+    wamerican \
     && \
     apt-get remove --purge -y \
     && \
@@ -23,53 +23,23 @@ RUN apt-get update \
     && \
     apt-get autoremove -y \
     && \
-    rm -rf /var/lib/apt/lists/* \
-    && \
-    mkdir -p /steamcmd \
-    && \
-    wget -qO- 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' | tar zxf - -C /steamcmd
-
-ENV TRACY_NO_INVARIANT_CHECK=1
+    rm -rf /var/lib/apt/lists/* 
 
 ENV STEAM_USER=""
 ENV STEAM_PASSWORD=""
 ENV STEAM_BRANCH="public"
 ENV STEAM_BRANCH_PASSWORD=""
 
-ENV ARMA_CONFIG=docker_generated
-ENV ARMA_PROFILE=/home/profile
+ENV ARMA_CONFIG=""
+ENV ARMA_PROFILE=/opt/ArmaReforgerServer/profile
 ENV ARMA_BINARY="./ArmaReforgerServer"
 ENV ARMA_PARAMS=""
 ENV ARMA_MAX_FPS=120
 
-ENV SERVER_REGION="EU"
-ENV SERVER_ID=""
-ENV SERVER_ADMIN_PASSWORD=""
-ENV SERVER_HOST_BIND_ADDRESS="0.0.0.0"
-ENV SERVER_HOST_BIND_PORT=2001
-ENV SERVER_HOST_REGISTER_ADDRESS=""
-ENV SERVER_HOST_REGISTER_PORT=2001
-
-ENV GAME_NAME="Arma Reforger Docker Server"
-ENV GAME_PASSWORD=""
-ENV GAME_SCENARIO_ID="{ECC61978EDCC2B5A}Missions/23_Campaign.conf"
-ENV GAME_PLAYER_LIMIT=32
-ENV GAME_AUTO_JOINABLE=false
-ENV GAME_VISIBLE=true
-ENV GAME_PROPS_BATTLEYE=true
-ENV GAME_PROPS_DISABLE_THIRD_PERSON=false
-ENV GAME_PROPS_FAST_VALIDATION=true
-ENV GAME_PROPS_SERVER_MAX_VIEW_DISTANCE=2500
-ENV GAME_PROPS_SERVER_MIN_GRASS_DISTANCE=50
-ENV GAME_PROPS_NETWORK_VIEW_DISTANCE=1000
-
 ENV SKIP_INSTALL=false
 
-WORKDIR /reforger
-
-VOLUME /steamcmd
-VOLUME /home/profile
-VOLUME /reforger/Configs
+WORKDIR /opt/ArmaReforgerServer
+VOLUME /opt/ArmaReforgerServer
 
 EXPOSE 2001/udp
 EXPOSE 17777/udp
@@ -77,6 +47,5 @@ EXPOSE 17777/udp
 STOPSIGNAL SIGINT
 
 COPY *.py /
-COPY docker_default.json /
 
 CMD ["python3","/launch.py"]
--- launch.py
+++ launch.py
@@ -1,131 +1,47 @@
-import json
 import os
-import random
 import subprocess
 
-CONFIG_GENERATED = "/reforger/Configs/docker_generated.json"
+STEAMCMD_PATH = "/opt/steamcmd"
+STEAMCMD_EXE = f"{STEAMCMD_PATH}/steamcmd.sh"
 
+# Install steamcmd.
+if not os.path.exists(STEAMCMD_EXE):
+    subprocess.call(f"mkdir -p {STEAMCMD_PATH}", shell=True)
+    subprocess.call(
+        f"wget -qO- 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' | tar zxf - -C {STEAMCMD_PATH}",
+        shell=True,
+    )
 
-def env_defined(key):
-    return key in os.environ and len(os.environ[key]) > 0
-
-
-def random_passphrase():
-    password = "'"
-    while "'" in password:
-        with open("/usr/share/dict/american-english") as f:
-            words = f.readlines()
-        password = "-".join(random.sample(words, 3)).replace("\n", "").lower()
-    return password
-
-
-def bool_str(text):
-    return text.lower() == "true"
-
-
+# Install/update the server.
 if os.environ["SKIP_INSTALL"] in ["", "false"]:
-    steamcmd = ["/steamcmd/steamcmd.sh"]
-    steamcmd.extend(["+force_install_dir", "/reforger"])
-    if env_defined("STEAM_USER"):
+    steamcmd = [STEAMCMD_EXE]
+    steamcmd.extend(["+force_install_dir", "/opt/ArmaReforgerServer"])
+    if os.environ.get("STEAM_USER"):
         steamcmd.extend(
             ["+login", os.environ["STEAM_USER"], os.environ["STEAM_PASSWORD"]]
         )
     else:
         steamcmd.extend(["+login", "anonymous"])
     steamcmd.extend(["+app_update", "1874900"])
-    if env_defined("STEAM_BRANCH"):
+    if os.environ.get("STEAM_BRANCH"):
         steamcmd.extend(["-beta", os.environ["STEAM_BRANCH"]])
-    if env_defined("STEAM_BRANCH_PASSWORD"):
+    if os.environ.get("STEAM_BRANCH_PASSWORD"):
         steamcmd.extend(["-betapassword", os.environ["STEAM_BRANCH_PASSWORD"]])
     steamcmd.extend(["validate", "+quit"])
     subprocess.call(steamcmd)
 
-if os.environ["ARMA_CONFIG"] != "docker_generated":
-    config_path = f"/reforger/Configs/{os.environ['ARMA_CONFIG']}"
-else:
-    if os.path.exists(CONFIG_GENERATED):
-        f = open(CONFIG_GENERATED)
-        config = json.load(f)
-        f.close()
-    else:
-        f = open("/docker_default.json")
-        config = json.load(f)
-        f.close()
-
-    if env_defined("SERVER_REGION"):
-        config["region"] = os.environ["SERVER_REGION"]
-    if env_defined("SERVER_ID"):
-        config["dedicatedServerId"] = os.environ["SERVER_ID"]
-    if env_defined("SERVER_ADMIN_PASSWORD"):
-        config["adminPassword"] = os.environ["SERVER_ADMIN_PASSWORD"]
-    else:
-        adminPassword = random_passphrase()
-        config["adminPassword"] = adminPassword
-        print(f"Admin password: {adminPassword}")
-    if env_defined("SERVER_HOST_BIND_ADDRESS"):
-        config["gameHostBindAddress"] = os.environ["SERVER_HOST_BIND_ADDRESS"]
-    if env_defined("SERVER_HOST_BIND_PORT"):
-        config["gameHostBindPort"] = int(os.environ["SERVER_HOST_BIND_PORT"])
-    if env_defined("SERVER_HOST_REGISTER_ADDRESS"):
-        config["gameHostRegisterBindAddress"] = os.environ[
-            "SERVER_HOST_REGISTER_ADDRESS"
-        ]
-    if env_defined("SERVER_HOST_REGISTER_PORT"):
-        config["gameHostRegisterPort"] = int(os.environ["SERVER_HOST_REGISTER_PORT"])
-
-    if env_defined("GAME_NAME"):
-        config["game"]["name"] = os.environ["GAME_NAME"]
-    if env_defined("GAME_PASSWORD"):
-        config["game"]["password"] = os.environ["GAME_PASSWORD"]
-    if env_defined("GAME_SCENARIO_ID"):
-        config["game"]["scenarioId"] = os.environ["GAME_SCENARIO_ID"]
-    if env_defined("GAME_PLAYER_LIMIT"):
-        config["game"]["playerCountLimit"] = int(os.environ["GAME_PLAYER_LIMIT"])
-    if env_defined("GAME_AUTO_JOINABLE"):
-        config["game"]["autoJoinable"] = bool_str(os.environ["GAME_AUTO_JOINABLE"])
-    if env_defined("GAME_VISIBLE"):
-        config["game"]["visible"] = bool_str(os.environ["GAME_VISIBLE"])
-    if env_defined("GAME_PROPS_BATTLEYE"):
-        config["game"]["gameProperties"]["battlEye"] = bool_str(
-            os.environ["GAME_PROPS_BATTLEYE"]
-        )
-    if env_defined("GAME_PROPS_DISABLE_THIRD_PERSON"):
-        config["game"]["gameProperties"]["disableThirdPerson"] = bool_str(
-            os.environ["GAME_PROPS_DISABLE_THIRD_PERSON"]
-        )
-    if env_defined("GAME_PROPS_FAST_VALIDATION"):
-        config["game"]["gameProperties"]["fastValidation"] = bool_str(
-            os.environ["GAME_PROPS_FAST_VALIDATION"]
-        )
-    if env_defined("GAME_PROPS_SERVER_MAX_VIEW_DISTANCE"):
-        config["game"]["gameProperties"]["serverMaxViewDistance"] = int(
-            os.environ["GAME_PROPS_SERVER_MAX_VIEW_DISTANCE"]
-        )
-    if env_defined("GAME_PROPS_SERVER_MIN_GRASS_DISTANCE"):
-        config["game"]["gameProperties"]["serverMinGrassDistance"] = int(
-            os.environ["GAME_PROPS_SERVER_MIN_GRASS_DISTANCE"]
-        )
-    if env_defined("GAME_PROPS_NETWORK_VIEW_DISTANCE"):
-        config["game"]["gameProperties"]["networkViewDistance"] = int(
-            os.environ["GAME_PROPS_NETWORK_VIEW_DISTANCE"]
-        )
-
-    f = open(CONFIG_GENERATED, "w")
-    json.dump(config, f, indent=4)
-    f.close()
-
-    config_path = CONFIG_GENERATED
-
+# Launch server.
 launch = " ".join(
     [
         os.environ["ARMA_BINARY"],
-        f"-config {config_path}",
         "-backendlog",
         "-nothrow",
+        f"-config {os.environ['ARMA_CONFIG']}",
         f"-maxFPS {os.environ['ARMA_MAX_FPS']}",
         f"-profile {os.environ['ARMA_PROFILE']}",
         os.environ["ARMA_PARAMS"],
     ]
 )
+
 print(launch, flush=True)
-os.system(launch)
+subprocess.call(launch, shell=True)
